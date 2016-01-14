7fca29664f45394edde32b82f1abf186503d6b2d
Jonas Bleyl <jonas.bleyl@gmail.com>
2016-01-14 21:20:03 +0000

    Improve date display format

--- app/src/main/java/com/bleyl/recurrence/utils/DateAndTimeUtil.java
+++ app/src/main/java/com/bleyl/recurrence/utils/DateAndTimeUtil.java
@@ -12,11 +12,11 @@ import java.util.Locale;
 public class DateAndTimeUtil {
 
     private static final SimpleDateFormat DATE_AND_TIME_FORMAT = new SimpleDateFormat("yyyyMMddHHmm", Locale.getDefault());
+    private static final SimpleDateFormat DATE_AND_TIME_WITH_SECONDS_FORMAT = new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault());
     private static final SimpleDateFormat READABLE_DAY_MONTH_FORMAT = new SimpleDateFormat("d MMMM", Locale.getDefault());
     private static final SimpleDateFormat READABLE_DAY_MONTH_YEAR_FORMAT = new SimpleDateFormat("d MMMM yyyy", Locale.getDefault());
     private static final SimpleDateFormat READABLE_TIME_24_FORMAT = new SimpleDateFormat("HH:mm", Locale.getDefault());
     private static final SimpleDateFormat READABLE_TIME_FORMAT = new SimpleDateFormat("h:mm a", Locale.getDefault());
-    private static final SimpleDateFormat READABLE_DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault());
     private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HHmm", Locale.getDefault());
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
     private static final SimpleDateFormat WEEK_DAYS_FORMAT = new SimpleDateFormat("EEEE", Locale.getDefault());
@@ -31,7 +31,8 @@ public class DateAndTimeUtil {
     }
 
     public static String toStringReadableDate(Calendar calendar) {
-        return READABLE_DATE_FORMAT.format(calendar.getTime());
+        java.text.DateFormat dateFormat = java.text.DateFormat.getDateInstance(java.text.DateFormat.FULL, Locale.getDefault());
+        return dateFormat.format(calendar.getTime());
     }
 
     public static String toStringReadableTime(Calendar calendar, Context context) {
@@ -48,6 +49,10 @@ public class DateAndTimeUtil {
 
     public static String toStringDateAndTime(Calendar calendar) {
         return DATE_AND_TIME_FORMAT.format(calendar.getTime());
+    }
+
+    public static String toStringDateTimeWithSeconds(Calendar calendar) {
+        return DATE_AND_TIME_WITH_SECONDS_FORMAT.format(calendar.getTime());
     }
 
     public static String getAppropriateDateFormat(Context context, Calendar calendar) {
