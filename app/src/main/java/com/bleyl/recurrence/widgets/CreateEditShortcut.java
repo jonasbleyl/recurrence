46d6f3246770acec339fefeacfbf96d0a06ec686
Jonas Bleyl <jonas.bleyl@gmail.com>
2016-04-14 07:59:20 +0100

    Automatically open keyboard in shortcut

--- app/src/main/java/com/bleyl/recurrence/widgets/CreateEditShortcut.java
+++ app/src/main/java/com/bleyl/recurrence/widgets/CreateEditShortcut.java
@@ -1,5 +1,24 @@
 package com.bleyl.recurrence.widgets;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.inputmethod.InputMethodManager;
+
 import com.bleyl.recurrence.activities.CreateEditActivity;
 
-public class CreateEditShortcut extends CreateEditActivity {}
+public class CreateEditShortcut extends CreateEditActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
+            }
+        }, 300);
+    }
+}
